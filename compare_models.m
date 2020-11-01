445d0cefc3f4707ce9e43f2b2a9842b6fe29a03c
hritz <harrison.ritz@gmail.com>
2020-11-01 11:55:11 -0500

    multi compare figures

--- compare_models.m
+++ compare_models.m
@@ -108,31 +108,32 @@ saveas(f_corr, './figures/corrplots.png')
 %% plot multivariate
 
 
-triState = {'NY', 'NJ', 'CT', 'PA'};
+compareStates = {'CT', 'ME', 'MA', 'NH', 'RI', 'VT'};
+nComp = length(compareStates);
 
 f_538 = figure('Renderer', 'painters', 'Position', [0 0 600 600]);
 
-[S_538,AX_538,BigAx_538,H_538,HAx_538] = plotmatrix(tbl538{datasample(1:nsims, 1e4),triState}, '.k');
-for ii = 1:4; H_538(ii).DisplayStyle = 'stairs';end
-for ii = 1:4; H_538(ii).EdgeColor = 'k';end
-for ii = 1:4; H_538(ii).LineWidth = 1;end
-for ii = 1:4; set(get(AX_538(1,ii),'Title'),'String',triState{ii});end
+[S_538,AX_538,BigAx_538,H_538,HAx_538] = plotmatrix(tbl538{datasample(1:nsims, 1e4),compareStates}, '.k');
+for ii = 1:nComp; H_538(ii).DisplayStyle = 'stairs';end
+for ii = 1:nComp; H_538(ii).EdgeColor = 'k';end
+for ii = 1:nComp; H_538(ii).LineWidth = 1;end
+for ii = 1:nComp; set(get(AX_538(1,ii),'Title'),'String',compareStates{ii});end
 
 
 f_Econ = figure('Renderer', 'painters', 'Position', [0 0 600 600]);
 
-[S_Econ,AX_Econ,BigAx_Econ,H_Econ,HAx_Econ] = plotmatrix(tblEcon{datasample(1:nsims, 1e4),triState}, '.r');
-for ii = 1:4; H_Econ(ii).DisplayStyle = 'stairs';end
-for ii = 1:4; H_Econ(ii).EdgeColor = 'r';end
-for ii = 1:4; H_Econ(ii).LineWidth = 1;end
-for ii = 1:4; set(get(AX_Econ(1,ii),'Title'),'String',triState{ii});end
-for ii = 1:4; HAx_Econ(ii).XLim = HAx_538(ii).XLim;end
-for ii = 1:16; AX_Econ(ii).XLim = AX_538(ii).XLim;end
-for ii = 1:16; AX_Econ(ii).YLim = AX_538(ii).YLim;end
+[S_Econ,AX_Econ,BigAx_Econ,H_Econ,HAx_Econ] = plotmatrix(tblEcon{datasample(1:nsims, 1e4),compareStates}, '.r');
+for ii = 1:nComp; H_Econ(ii).DisplayStyle = 'stairs';end
+for ii = 1:nComp; H_Econ(ii).EdgeColor = 'r';end
+for ii = 1:nComp; H_Econ(ii).LineWidth = 1;end
+for ii = 1:nComp; set(get(AX_Econ(1,ii),'Title'),'String',compareStates{ii});end
+for ii = 1:nComp; HAx_Econ(ii).XLim = HAx_538(ii).XLim;end
+for ii = 1:(nComp^2); AX_Econ(ii).XLim = AX_538(ii).XLim;end
+for ii = 1:(nComp^2); AX_Econ(ii).YLim = AX_538(ii).YLim;end
 
 
-saveas(f_538, './figures/tristate_538.png') 
-saveas(f_Econ, './figures/tristate_Econ.png') 
+saveas(f_538, './figures/compareMulti_538.png') 
+saveas(f_Econ, './figures/compareMulti_Econ.png') 
 
 
 
